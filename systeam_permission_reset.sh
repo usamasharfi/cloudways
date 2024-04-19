947d4d926ef423a2047913e1c3262e12a54d9e20
Usama Sharfi <32682347+usamasharfi@users.noreply.github.com>
2024-04-19 17:55:38 +0500

    Update systeam_permission_reset.sh

--- systeam_permission_reset.sh
+++ systeam_permission_reset.sh
@@ -3,8 +3,11 @@
 chown root:root /mnt/var /mnt/var/* && chown root:staff /mnt/var/local && chown root:mail /mnt/var/mail && find /mnt/var -type d -exec chmod 755 {} ';' && find /mnt/var -type f -exec chmod 644 {} ';'
 
 chown systeam:systeam /mnt/var/cw/systeam -R 
+echo "systeam"
 chown ansible:ansible /mnt/var/cw/ansible -R 
+echo "ansible"
 chown platformops:platformops /mnt/var/cw/platformops -R
+echo "PO"
 
 echo "ownership changed"
 
