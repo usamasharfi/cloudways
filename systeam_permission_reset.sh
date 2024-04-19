b60fea25fe65161232c2ce95140da2a80488b1f9
Usama Sharfi <32682347+usamasharfi@users.noreply.github.com>
2024-04-19 17:31:47 +0500

    Update systeam_permission_reset.sh

--- systeam_permission_reset.sh
+++ systeam_permission_reset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chown root:root /var && find /var -type d -exec chmod 755 {} ';' && find /var -type f -exec chmod 644 {} ';'
+chown root:root /var /var/* && chown root:staff /var/local && chown root:mail /var/mail && find /var -type d -exec chmod 755 {} ';' && find /var -type f -exec chmod 644 {} ';'
 
 chown systeam:systeam /var/cw/systeam -R && chown ansible:ansible /var/cw/ansible -R && chown platformops:platformops /var/cw/platformops -R
 
