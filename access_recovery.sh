475b3c59ef9ef99dfc64a0c27ecd87a8302a7a8e
Usama Sharfi <32682347+usamasharfi@users.noreply.github.com>
2024-04-21 15:29:28 +0500

    Update and rename systeam_permission_reset.sh to access_recovery.sh

--- access_recovery.sh
+++ access_recovery.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
 chown root:root /mnt/var /mnt/var/* && chown root:staff /mnt/var/local && chown root:mail /mnt/var/mail && find /mnt/var -type d -exec chmod 755 {} ';' && find /mnt/var -type f -exec chmod 644 {} ';'
-
-chown systeam:systeam /mnt/var/cw/systeam -R 
-echo "systeam"
-chown ansible:ansible /mnt/var/cw/ansible -R 
-echo "ansible"
-chown platformops:platformops /mnt/var/cw/platformops -R
-echo "PO"
-
-echo "ownership changed"
-
 chmod 750 /mnt/var/cw/systeam/.openssh && chmod 640 /mnt/var/cw/systeam/.openssh/authorized_keys
 
-echo "permission changed"
