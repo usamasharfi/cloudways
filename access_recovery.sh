2f7cd2683ad71047125b6ca331685aa1177a7fb1
Usama Sharfi <32682347+usamasharfi@users.noreply.github.com>
2024-04-21 17:51:31 +0500

    Update access_recovery.sh

--- access_recovery.sh
+++ access_recovery.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-chown root:root /mnt/var /mnt/var/* && chown root:staff /mnt/var/local && chown root:mail /mnt/var/mail && find /mnt/var -type d -exec chmod 755 {} ';' && find /mnt/var -type f -exec chmod 644 {} ';'
-chmod 750 /mnt/var/cw/systeam/.openssh && chmod 640 /mnt/var/cw/systeam/.openssh/authorized_keys
+chown root:root /mnt/var /mnt/var/* && chown root:staff /mnt/var/local && chown root:mail /mnt/var/mail -R
+find /mnt/var -type d -exec chmod 755 {} ';' && find /mnt/var -type f -exec chmod 644 {} ';'
 
+#chmod 755 /mnt/var/cw/systeam/.openssh && chmod 644 /mnt/var/cw/systeam/.openssh/authorized_keys
